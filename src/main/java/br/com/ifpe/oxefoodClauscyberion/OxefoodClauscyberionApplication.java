22fada1b60972330486e4119391f3f4f4b4fbcba
aluno <aluno@ifpe>
2022-09-22 20:49:32 -0300

    projeto de web 4

---
+++ src/main/java/br/com/ifpe/oxefoodClauscyberion/OxefoodClauscyberionApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodClauscyberion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodClauscyberionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodClauscyberionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodClauscyberion/OxefoodClauscyberionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodClauscyberion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodClauscyberionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
